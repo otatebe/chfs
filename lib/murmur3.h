addd3ac60dacec598d03e4ad9278ae7cde5da7e1
Osamu Tatebe <tatebe@cs.tsukuba.ac.jp>
2021-04-26 06:37:17 +0000

    style fix by clang-format
    git-svn-id: svn+ssh://platina.ccs.tsukuba.ac.jp/svnroot/home/trunk/mochi/chfs@2574 7c857e19-e864-4221-8d65-6e103b07b3e5

--- lib/murmur3.h
+++ lib/murmur3.h
@@ -1,8 +1,8 @@
-//-----------------------------------------------------------------------------
-// MurmurHash3 was written by Austin Appleby, and is placed in the
-// public domain. The author hereby disclaims copyright to this source
-// code.
-
+/*-----------------------------------------------------------------------------
+ * MurmurHash3 was written by Austin Appleby, and is placed in the
+ * public domain. The author hereby disclaims copyright to this source
+ * code.
+ */
 #ifndef _MURMURHASH3_H_
 #define _MURMURHASH3_H_
 
@@ -12,18 +12,18 @@
 extern "C" {
 #endif
 
-//-----------------------------------------------------------------------------
-
-void MurmurHash3_x86_32 (const void *key, int len, uint32_t seed, void *out);
+/*-----------------------------------------------------------------------------
+ */
+void MurmurHash3_x86_32(const void *key, int len, uint32_t seed, void *out);
 
 void MurmurHash3_x86_128(const void *key, int len, uint32_t seed, void *out);
 
 void MurmurHash3_x64_128(const void *key, int len, uint32_t seed, void *out);
 
-//-----------------------------------------------------------------------------
-
+/*-----------------------------------------------------------------------------
+ */
 #ifdef __cplusplus
 }
 #endif
 
-#endif // _MURMURHASH3_H_
+#endif /* _MURMURHASH3_H_ */
