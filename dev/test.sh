4eee7a79be9e12d77cb8bf9b2930ade7b49c6097
Osamu Tatebe <tatebe@cs.tsukuba.ac.jp>
2022-10-13 12:14:31 +0900

    (dev/test.sh) fix ls -l test

--- dev/test.sh
+++ dev/test.sh
@@ -12,8 +12,8 @@ diff ~/chfs/configure $MDIR/configure
 s1=$(wc ~/chfs/configure | awk '{ print $3 }')
 s2=$(wc $MDIR/configure | awk '{ print $3 }')
 [ $s1 = $s2 ]
-s1=$(ls -l ~/chfs/configure | awk '{ print $4 }')
-s2=$(ls -l $MDIR/configure | awk '{ print $4 }')
+s1=$(ls -l ~/chfs/configure | awk '{ print $5 }')
+s2=$(ls -l $MDIR/configure | awk '{ print $5 }')
 [ $s1 = $s2 ]
 chfind
 mpirun -x PATH -x CHFS_SERVER -np 4 -hostfile hosts -map-by node ior -a CHFS -o /tmp/a/test -g
