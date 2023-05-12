56d49f55ee44aa5a2292900fea36a79659c60719
Osamu Tatebe <tatebe@cs.tsukuba.ac.jp>
2023-05-12 19:29:17 +0900

    (dev/install.sh) parallel make and pmemkv option

--- dev/install.sh
+++ dev/install.sh
@@ -2,11 +2,21 @@
 
 set -eu
 
+OPT=
+while [ $# -gt 0 ]
+do
+	case $1 in
+	pmemkv) OPT=--with-pmemkv ;;
+	*) exit 1 ;;
+	esac
+	shift
+done
+
 . $HOME/spack/share/spack/setup-env.sh
 spack load mochi-margo
 cd ~/chfs
 autoreconf -i
-./configure --prefix $HOME/local > /dev/null
+./configure --prefix $HOME/local $OPT > /dev/null
 make clean > /dev/null
-make > /dev/null
+make -j $(nproc) > /dev/null
 make install > /dev/null
