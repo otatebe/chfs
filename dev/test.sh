d90e157384cf11459ff97c01daa34c63ce281dd2
Osamu Tatebe <tatebe@cs.tsukuba.ac.jp>
2024-01-18 12:24:23 +0900

    (dev/test.sh) no major change

--- dev/test.sh
+++ dev/test.sh
@@ -7,7 +7,7 @@ MDIR=/tmp/a
 
 # clean up
 chfsctl -h hosts -m $MDIR stop 2> /dev/null
-chfsctl -h hosts -m $MDIR clean
+chfsctl -h hosts clean
 
 eval $(chfsctl -h hosts -m $MDIR -L log start)
 chlist
@@ -19,8 +19,8 @@ s2=$(wc $MDIR/configure | awk '{ print $3 }')
 s1=$(ls -l ~/chfs/configure | awk '{ print $5 }')
 s2=$(ls -l $MDIR/configure | awk '{ print $5 }')
 [ $s1 = $s2 ]
-chfind
-mpirun -x PATH -x CHFS_SERVER -np 4 -hostfile hosts -map-by node ior -a CHFS -o /tmp/a/test -g
+chfind $MDIR
+mpirun -x PATH -x CHFS_SERVER -np 4 -hostfile hosts -map-by node ior -a CHFS -o $MDIR/test -g -w -r -R -G 12345
 chfsctl -h hosts -m $MDIR stop
 
 echo OK
