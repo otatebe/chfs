c5931fcf37f9c8e5d64544dca5c5e69fb44ac98b
Osamu Tatebe <tatebe@cs.tsukuba.ac.jp>
2024-01-18 12:24:23 +0900

    (dev/test.sh) no major change

--- dev/test.sh
+++ dev/test.sh
@@ -4,11 +4,11 @@ set -eux
 
 LANG=C
 MDIR=/tmp/a
-BACKEND=$HOME/chfs/dev/backend
+BACKEND=$PWD/backend
 
 # clean up
 chfsctl -h hosts -m $MDIR stop 2> /dev/null
-chfsctl -h hosts -m $MDIR clean
+chfsctl -h hosts clean
 
 eval $(chfsctl -h hosts -m $MDIR -b $BACKEND -f 2 -L log start)
 chlist
@@ -20,7 +20,7 @@ s2=$(wc $MDIR/configure | awk '{ print $3 }')
 s1=$(ls -l ~/chfs/configure | awk '{ print $5 }')
 s2=$(ls -l $MDIR/configure | awk '{ print $5 }')
 [ $s1 = $s2 ]
-chfind
+chfind $MDIR
 mpirun -x PATH -x CHFS_SERVER -x CHFS_BACKEND_PATH -x CHFS_SUBDIR_PATH -np 4 -hostfile hosts -map-by node ior -a CHFS -o $MDIR/test -g -w -r -R -G 12345 -k
 chfsctl -h hosts -m $MDIR stop
 
