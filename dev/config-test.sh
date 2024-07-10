3161534240122e8af841b58c92ba9f8678d979c8
Osamu Tatebe <tatebe@cs.tsukuba.ac.jp>
2024-07-11 00:12:34 +0900

    (dev) config-test.sh - --disable-modular-hashing and --enable-digest-murmur3.  execute test.sh for a regression test

--- dev/config-test.sh
+++ dev/config-test.sh
@@ -6,21 +6,24 @@ cd $HOME/chfs
 autoreconf -i
 
 echo PMEMKV backend
-for hashing in " " --enable-modular-hashing
+for hashing in " " --disable-modular-hashing
 do
 	for port in " " --enable-hash-port
 	do
 		for zero in " " --enable-zero-copy-read-rdma
 		do
-			for md5 in " " --enable-digest-md5
+			for md5 in " " --enable-digest-md5 \
+				--enable-digest-murmur3
 			do
 				echo ./configure --with-pmemkv \
 					$hashing $port $zero $md5
 				rm -rf build && mkdir build && cd build
-				../configure --with-pmemkv \
+				../configure --prefix $HOME/local \
+					--with-pmemkv \
 					$hashing $port $zero $md5 > /dev/null
-				make clean > /dev/null
 				make -j $(nproc) > /dev/null
+				make install > /dev/null
+				(cd ../dev && sh ./test.sh)
 				cd ..
 			done
 		done
@@ -28,7 +31,7 @@ do
 done
 
 echo POSIX backend
-for hashing in " " --enable-modular-hashing
+for hashing in " " --disable-modular-hashing
 do
 	for port in " " --enable-hash-port
 	do
@@ -36,15 +39,18 @@ do
 		do
 #			for abtio in " " --with-abt-io
 #			do
-				for md5 in " " --enable-digest-md5
+				for md5 in " " --enable-digest-md5 \
+					--enable-digest-murmur3
 				do
 					echo ./configure $hashing $port \
 						$xattr $abtio $md5
 					rm -rf build && mkdir build && cd build
-					../configure $hashing $port \
+					../configure --prefix $HOME/local\
+						$hashing $port \
 						$xattr $abtio $md5 > /dev/null
-					make clean > /dev/null
 					make -j $(nproc) > /dev/null
+					make install > /dev/null
+					(cd ../dev && sh ./test.sh)
 					cd ..
 				done
 #			done
