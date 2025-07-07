60df52a57fd1b63f585d6e516313e4ba927d50c5
Osamu Tatebe <tatebe@cs.tsukuba.ac.jp>
2025-07-07 21:44:58 +0900

    (dev) pmemkv backend requires -U 5 for test/test

--- dev/test-start.sh
+++ dev/test-start.sh
@@ -5,5 +5,5 @@ chfsctl -h hosts -m $MDIR stop 2> /dev/null
 chfsctl -h hosts clean
 
 # chfsctl start
-eval $(chfsctl -h hosts -m $MDIR -b $BACKEND -f 2 -L log start)
+eval $(chfsctl -h hosts -m $MDIR -b $BACKEND -f 2 -L log -O "-U 5" start)
 chlist
